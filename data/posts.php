2a8e52344b9cb93435f25d3156424f5f37426fb3
Jorge Mendes <jorge.mendes7@hotmail.com>
2025-05-11 19:56:20 -0300

    Criado o mock para os posts

--- data/posts.php
+++ data/posts.php
@@ -3,8 +3,26 @@
     $posts = [
         [
             'id' => 1,
-            'title' => 'Lorem ipsum dolor',
-            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa.',
-            'img' => 'post1'
+            'title' => 'Conheça os Principais Tipos de Cerveja',
+            'description' => 'Uma introdução ao universo cervejeiro, explicando a diferença entre os principais tipos de cerveja como Lager, Ale, Pilsen, Weiss, IPA e muito mais. Ideal para quem está começando a explorar esse mundo.',
+            'img' => 'post1.jpg'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Copo Ideal: Qual Usar para Cada Tipo de Cerveja?',
+            'description' => ' Entenda como o formato do copo influencia na experiência de degustação e descubra qual é o copo ideal para cada estilo de cerveja.',
+            'img' => 'post2.jpg'
+        ],
+        [
+            'id' => 3,
+            'title' => ' American IPA: A Intensidade do Lúpulo',
+            'description' => 'Um mergulho no estilo mais querido pelos hopheads. Saiba como surgiu a American IPA, suas características marcantes e por que ela conquistou o paladar de tantos cervejeiros.',
+            'img' => 'post3.jpg'
+        ],
+        [
+            'id' => 4,
+            'title' => 'Stout: Potência, Sabor e História em Cada Gole',
+            'description' => 'Forte, encorpada e cheia de personalidade. Conheça a história, os tipos e as curiosidades por trás do estilo Stout, a cerveja que vai muito além do amargor.',
+            'img' => 'post1.jpg'
         ]
     ];
