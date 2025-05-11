8bc53a30993b1532a31eaaf0aba0aa2bbefe722b
Jorge Mendes <jorge.mendes7@hotmail.com>
2025-05-10 23:27:03 -0300

    adicionado mock com os dados dos posts

---
+++ data/posts.php
@@ -0,0 +1,10 @@
+<?php
+
+    $posts = [
+        [
+            'id' => 1,
+            'title' => 'Lorem ipsum dolor',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa.',
+            'img' => 'post1'
+        ]
+    ];
