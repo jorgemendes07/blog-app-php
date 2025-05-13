e4161ca2e4d391167a924330d92ef9232b0bc6c0
Jorge Mendes <jorge.mendes7@hotmail.com>
2025-05-13 20:48:25 -0300

    adicionado estilo a pagina sobre.php

--- sobre.php
+++ sobre.php
@@ -2,14 +2,13 @@
     include_once('./templates/header.php');
 ?>
 
-<main class="sobre-container">
+<main id="sobre-container">
     <h1 id="sobre-title">Sobre o projeto</h1>
-    <p>
-    Este blog foi desenvolvido com o objetivo de praticar meus conhecimentos em PHP, utilizando dados mockados para simular um banco de dados e gerar páginas de forma dinâmica.
-
-    A estrutura conta com templates reutilizáveis (header e footer), carregamento dinâmico dos posts via id e organização modular dos arquivos. Ao clicar em um post, o conteúdo completo é exibido em uma página individual.
-
-    Escolhi o tema cerveja artesanal para tornar o projeto mais leve e atrativo. O foco principal é demonstrar minha capacidade de aplicar lógica em PHP puro e estruturar um site funcional com múltiplas páginas.
+    <p>Este blog foi desenvolvido com o objetivo de praticar meus conhecimentos em PHP, utilizando dados mockados para simular um banco de dados e gerar páginas de forma dinâmica. </p>
+    <br>
+    <p>A estrutura conta com templates reutilizáveis (header e footer), carregamento dinâmico dos posts via id e organização modular dos arquivos. Ao clicar em um post, o conteúdo completo é exibido em uma página individual.</p>
+    <br>
+    <p>Escolhi o tema cerveja artesanal para tornar o projeto mais leve e atrativo. O foco principal é demonstrar minha capacidade de aplicar lógica em PHP puro e estruturar um site funcional com múltiplas páginas.</p>
 </p>
 </main>
 
